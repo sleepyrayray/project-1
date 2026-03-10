619c120314d923f36af65c22b2656363033c11da
Ray Hernaez <r.hernz@outlook.com>
2026-03-09 21:46:55 -0400

    Implemented Game Start
    - Implemented Game Start screen
    - User needs to press spacebar to start game

--- js/Game.js
+++ js/Game.js
@@ -8,6 +8,7 @@ class Game {
 
         // Game state
         this.caught = false;
+        this.gameStarted = false;
         this.thiefCollectCount = 0;
         this.thiefCollectGoal = 15;
         this.thiefWon = false;
@@ -418,6 +419,13 @@ class Game {
     }
 
     update(dt) {
+        if (!this.gameStarted) {
+            if (this.input.isDown(" ")) {
+                this.gameStarted = true;
+            }
+            return;
+        }
+
         if (this.gameOver) {
             return;
         }
@@ -481,6 +489,23 @@ class Game {
     }
 
     draw() {
+        if (!this.gameStarted) {
+
+            this.ctx.fillStyle = "black";
+            this.ctx.fillRect(0, 0, this.width, this.height);
+
+            this.ctx.font = "bold 40px sans-serif";
+            this.ctx.fillStyle = "white";
+            this.ctx.textAlign = "center";
+
+            this.ctx.fillText("POLICE VS THIEF", this.width / 2, this.height / 2 - 40);
+
+            this.ctx.font = "24px sans-serif";
+            this.ctx.fillText("Press SPACE to Start", this.width / 2, this.height / 2 + 20);
+
+            return;
+        }
+
         this.tileMap.draw(this.ctx, this.width, this.height);
 
         for (const c of this.collectibles) {
